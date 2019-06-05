8d74094b5dc58453522bce9734b0a4cc3ba44a25
sachin-me <sachinkumartiwari363@gmail.com>
2019-06-05 20:25:34 +0530

    add functionality, got some bugs to fix

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+
+require('dotenv').config();
+const APIAI_TOKEN = 'e3642b297a043d0b30a8a4f4f3f7d62a1ddef473';
+const APIAI_SESSION_ID = 'd1b31d6105044cd7b30a78f053ce883891556201069';
+
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
@@ -12,6 +17,38 @@ const server = app.listen(port, () => {
 	console.log(`server is running on ${port}`);
 })
 
+const io = require('socket.io')(server);
+io.on('connection', function(socket) {
+	console.log('A user connected');
+})
+
+const apiai = require('apiai')(APIAI_TOKEN);
+
+// web ui
 app.get('/', (req, res) => {
 	res.sendFile('index.html');
+})
+
+io.on('connection', function(socket) {
+	socket.on('chat message', (text) => {
+		console.log(`message: ${text}`);
+
+		// Getting reply from ai
+		let apiaiReq = apiai.textRequest(text, {
+			sessionId: APIAI_SESSION_ID
+		})
+
+		apiaiReq.on('response', (response) => {
+			let aiText = response.result.fulfillment.speech;
+			console.log(`Bot reply: ${aiText}`);
+
+			socket.emit('bot reply', aiText);
+		})
+
+		apiaiReq.on('error', (error) => {
+			console.log(`Getting error: ${error}`);
+		})
+
+		apiaiReq.end();
+	})
 })
--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,16 @@
+
+const socket = io();
+
+const you = document.querySelector('.you');
+const bot = document.querySelector('.bot');
+
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
 const recognition = new SpeechRecognition();
 
 // setting customize speech recognition language 
 recognition.lang = 'en-US';
 recognition.interimResults = false;
+recognition.maxAlternatives = 1;
 
 let talkBtn = document.querySelector('button');
 
@@ -12,6 +19,38 @@ talkBtn.addEventListener('click', () => {
 	recognition.start();
 }) 
 
+recognition.addEventListener('speechstart', () => {
+	console.log('Speech has been started');
+})
+
 recognition.addEventListener('result', (e) => {
-	console.log(e, 'checking event');
+	console.log('Result has been detected');
+	let last = e.results.length - 1;
+	let text = e.results[last][0].transcript;
+
+	you.textContent = text;
+	console.log('Confidence: ', e.results[0][0].confidence);
+	socket.emit('chat message', text);
+})
+
+recognition.addEventListener('speechend', () => {
+	recognition.stop();
+})
+
+recognition.addEventListener('error', (e) => {
+	bot.textContent = `Error: ${e.error}`; 
+})
+
+function synthVoice(text) {
+	const synth = window.speechSynthesis;
+	const utterance = new SpeechSynthesisUtterance();
+	utterance.text = text;
+	synth.speak(utterance);
+}
+
+socket.on('bot reply', (replyText) => {
+	synthVoice(replyText);
+
+	if (replyText == '') replyText = 'No Answer...';
+	bot.textContent = replyText;
 })
